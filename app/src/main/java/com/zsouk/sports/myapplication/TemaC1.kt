2addc74d83a8b3f3ee23107830975d6e18660904
hubengos <kosinad@gasos-ro.cz>
2023-03-16 20:15:35 +0100

    Scrolling

---
+++ app/src/main/java/com/zsouk/sports/myapplication/TemaC1.kt
@@ -0,0 +1,11 @@
+package com.zsouk.sports.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TemaC1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tema_c1)
+    }
+}
